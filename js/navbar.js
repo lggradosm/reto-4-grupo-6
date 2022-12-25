b8b384d8a8a0a50e20f2bad384147d9887c8dcda
lggradosm <gabrielgradosmachado@gmail.com>
2022-12-24 20:32:11 -0500

    FIX: image relatives routes

--- js/navbar.js
+++ js/navbar.js
@@ -3,10 +3,18 @@ export const navbar = () => {
   const $menu_trigger = $("menu-trigger");
   const $menu = $("menu");
   const $menu_close = $("close-menu");
+  const $body = document.querySelector("body");
+
+  let cartOpen = false;
+
   $menu_trigger.addEventListener("click", () => {
+    cartOpen = !cartOpen;
+    $body.style.overflow = "hidden";
     $menu.classList.add("active");
   });
   $menu_close.addEventListener("click", () => {
+    cartOpen = !cartOpen;
+    $body.style.overflow = "auto";
     $menu.classList.remove("active");
   });
 };
--- js/productsList.js
+++ js/productsList.js
@@ -3,89 +3,197 @@ import { initSwiper } from "./swiper.js";
 export const productsList = () => {
   const $ = (id) => document.getElementById(id);
   const $product_grid = $("product_grid");
-
+  const $btnCart = $("btnCart");
+  const $cart = $("cart");
+  const $cart_close = document.querySelector("#cart .cart__content--close");
+  const $body = document.querySelector("body");
+  const $cartItems = $("cartItems");
+  const $total = $("total");
+  let cartCounter = 0;
+  let cartList = [];
+  let total = 0;
+  let cartOpen = false;
   const generateStars = (stars) => {
     let starsHtml = "";
-    for (let i = 0; i < 5; i++) {
-      if (i < stars) {
-        starsHtml = starsHtml + '<i class="bi-star-fill"></i>';
-      } else {
-        starsHtml = starsHtml + '<i class="bi-star"></i>';
-      }
-    }
+    for (let i = 0; i < 5; i++)
+      i < stars
+        ? (starsHtml = starsHtml + '<i class="bi-star-fill"></i>')
+        : (starsHtml = starsHtml + '<i class="bi-star"></i>');
     return starsHtml;
   };
 
   const clickHandler = (index) => {
-    console.log(index);
+    let product = products[index];
+    cartCounter++;
+    renderCartCounterHtml();
+    addProductToCart(product);
   };
 
-  let html = products.reduce((acumulator, product) => {
-    return (
-      acumulator +
-      ` <div class="product__item">
-    <div class="swiper swiper__product">
-      <div class="swiper-wrapper">
-      ${product.img.reduce((acc, image, index, array) => {
-        return (
-          acc +
-          `<div class="${
-            array.length > 1 ? "swiper-slide" : ""
-          } "><img class="product__image ${
-            !product.stock > 0 ? "filter-gray" : ""
-          }" src="${image}" alt=""/></div> `
-        );
-      }, "")}
+  const btnCartClickHandler = () => {
+    $btnCart.addEventListener("click", () => {
+      cartOpen = !cartOpen;
+      cartOpen
+        ? ($body.style.overflow = "hidden")
+        : ($body.style.overflow = "auto");
+      $cart.classList.toggle("cart--visible");
+      renderCartDetailHtml();
+    });
+    $cart_close.addEventListener("click", () => {
+      cartOpen = !cartOpen;
+      $body.style.overflow = "auto";
+      $cart.classList.remove("cart--visible");
+    });
+  };
+
+  const addProductToCart = (product) => {
+    cartList.push(product);
+    product.discount
+      ? (total += (product.price * (100 - product.discount)) / 100)
+      : (total += product.price);
+  };
+
+  const removeProductToCart = (index) => {
+    const product = cartList[index];
+    product.discount
+      ? (total -= (product.price * (100 - product.discount)) / 100)
+      : (total -= product.price);
+    cartList.splice(index, 1);
+    cartCounter--;
+    renderCartCounterHtml();
+    renderCartDetailHtml();
+  };
+
+  const renderCartCounterHtml = () => {
+    $btnCart.innerHTML =
+      cartCounter > 0
+        ? ` <span class="cart__indicator" >${cartCounter}</span>`
+        : "";
+  };
+
+  const renderCartDetailHtml = () => {
+    let cartHtml = cartList.reduce((acc, item) => {
+      return (
+        acc +
+        `<div class="cart__item" > <div class="cart__item-image">
+        <img src="${item.img[0]}" alt="" />
+      </div>
+      <div class="cart__item-body flex-1">
+        <h4 class="product__name ">${item.name}</h4>
+        <p class="product__description">
+          ${item.description}
+        </p>
+        <div class="product__cost f-elements gap-sm ">
+            <p class="product__price  ${
+              item.discount ? "text-crossed" : "text-red text-bold"
+            }">S/ ${item.price.toFixed(2)}</p>
+            ${
+              item.discount
+                ? '<p class="product__discount">-' +
+                  item.discount +
+                  '%</p><p class="product__finalPrice">S/ ' +
+                  ((item.price * (100 - item.discount)) / 100).toFixed(2) +
+                  "</p>"
+                : ""
+            }
+            
+          </div>
+      </div>
+
+      <div class="cart__item-action" id="btnRemove">
+        <i class="bi-trash-fill"></i>
+      </div> </div>`
+      );
+    }, "");
+    $cartItems.innerHTML = cartHtml;
+    $total.innerHTML = total.toFixed(2);
+    const $btnRemove = document.querySelectorAll("#btnRemove");
+    $btnRemove.forEach(($option, index) => {
+      $option.addEventListener("click", () => removeProductToCart(index));
+    });
+  };
+
+  const renderProducts = () => {
+    let html = products.reduce((acumulator, product) => {
+      return (
+        acumulator +
+        ` <div class="product__item">
+      <div class="swiper swiper__product">
+        <div class="swiper-wrapper">
+        ${product.img.reduce((acc, image, index, array) => {
+          return (
+            acc +
+            `<div class="${
+              array.length > 1 ? "swiper-slide" : ""
+            } "><img class="product__image ${
+              !product.stock > 0 ? "filter-gray" : ""
+            }" src="${image}" alt=""/></div> `
+          );
+        }, "")}
+         
+        </div>
+        ${
+          product.img.length > 1
+            ? ' <div class="product__image-button-left"><i class="bi-chevron-left"></i></div><div class="product__image-button-right"><i class="bi-chevron-right"></i></div><div class="swiper-pagination"></div>'
+            : ""
+        }
        
       </div>
       ${
-        product.img.length > 1
-          ? ' <div class="product__image-button-left"><i class="bi-chevron-left"></i></div><div class="product__image-button-right"><i class="bi-chevron-right"></i></div><div class="swiper-pagination"></div>'
+        !product.stock > 0
+          ? ' <div class="product__sold-out">AGOTADO</div>'
           : ""
       }
-     
-    </div>
-    ${!product.stock > 0 ? ' <div class="product__sold-out">AGOTADO</div>' : ""}
-    <div class="product__content">
-        <h2 class="product__name ">${product.name}</h2>
-        <div class="product__cost f-elements gap-sm ">
-          <p class="product__price  ${
-            product.discount ? "text-crossed" : "text-red text-bold"
-          }">S/ ${product.price.toFixed(2)}</p>
+      <div class="product__content">
+          <h2 class="product__name ">${product.name}</h2>
+          <div class="product__cost f-elements gap-sm ">
+            <p class="product__price  ${
+              product.discount ? "text-crossed" : "text-red text-bold"
+            }">S/ ${product.price.toFixed(2)}</p>
+            ${
+              product.discount
+                ? '<p class="product__discount">-' +
+                  product.discount +
+                  '%</p><p class="product__finalPrice">S/ ' +
+                  ((product.price * (100 - product.discount)) / 100).toFixed(
+                    2
+                  ) +
+                  "</p>"
+                : ""
+            }
+            
+          </div>
+  
+        <p class="product__description">
+          ${product.description}
+        </p>
+        <div class="product__rating">
           ${
-            product.discount
-              ? '<p class="product__discount">-' +
-                product.discount +
-                '%</p><p class="product__finalPrice">S/ ' +
-                ((product.price * (100 - product.discount)) / 100).toFixed(2) +
-                "</p>"
-              : ""
+            product.stars > 0
+              ? generateStars(product.stars)
+              : '<i class="bi-star"></i><i class="bi-star"></i><i class="bi-star"></i><i class="bi-star"></i><i class="bi-star"></i>'
           }
-          
+        
         </div>
+        <div class="product__button f-elements">
+          <button id='btnAdd' class="">Agregar</button>
+        </div>
+      </div>
+    </div>`
+      );
+    }, "");
+    $product_grid.innerHTML = html;
+    const $btnAdd = document.querySelectorAll("#btnAdd");
+    $btnAdd.forEach(($option, index) => {
+      $option.addEventListener("click", () => clickHandler(index));
+    });
+    initSwiper();
+  };
 
-      <p class="product__description">
-        ${product.description}
-      </p>
-      <div class="product__rating">
-        ${
-          product.stars > 0
-            ? generateStars(product.stars)
-            : '<i class="bi-star"></i><i class="bi-star"></i><i class="bi-star"></i><i class="bi-star"></i><i class="bi-star"></i>'
-        }
-      
-      </div>
-      <div class="product__button f-elements">
-        <button id='btnAdd' class="">Agregar</button>
-      </div>
-    </div>
-  </div>`
-    );
-  }, "");
-  $product_grid.innerHTML = html;
-  const $btnAdd = document.querySelectorAll("#btnAdd");
-  $btnAdd.forEach(($option, index) => {
-    $option.addEventListener("click", () => clickHandler(index));
-  });
-  initSwiper();
+  const init = () => {
+    if (cartCounter > 0) renderCartCounterHtml();
+    renderProducts();
+    btnCartClickHandler();
+  };
+
+  init();
 };
